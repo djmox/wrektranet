437f8a912a39f86d7a6b7888ca4b5482247f9ffc
coma <jmoxley@gatech.edu>
2019-08-27 12:50:49 -0400

    gpg key fix

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ sudo chmod o+r /etc/profile.d/oracle.sh
 sudo ln -s /usr/include/oracle/$ORACLE_CLIENT_VERSION/client64 /usr/lib/oracle/$ORACLE_CLIENT_VERSION/client64/include
 source /etc/profile.d/oracle.sh
 
-gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable --ruby
 source ~/.rvm/scripts/rvm
 
